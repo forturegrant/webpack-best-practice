574582a1e14283a103d91b059c06cf481450d810
zhuangzipeng <wb-zzp809023@alibaba-inc.com>
2022-08-29 15:04:50 +0800

    feat: useImperativeHandle

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './index.less';
+import Father from './useImperativeHandle';
 
 export default class App extends React.Component {
     render() {
-        return <div className='header'>222</div>
+        return <div>
+            <div className='header'>222</div>
+            <Father />
+        </div>
     }
 }
