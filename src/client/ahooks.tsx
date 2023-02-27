37c882dc1ff2792a34d88ed762ed87dce696b815
forturegrant <1209095474@qq.com>
2023-02-27 17:42:45 +0800

    feat: update babel-config

---
+++ src/client/ahooks.tsx
@@ -0,0 +1,43 @@
+import { useBoolean, useRequest } from 'ahooks';
+import Mock from 'mockjs';
+import React from 'react';
+
+async function getArticle(): Promise<{ data: string; time: number }> {
+  console.log('cacheKey');
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: Mock.mock('@paragraph'),
+        time: new Date().getTime(),
+      });
+    }, 1000);
+  });
+}
+
+const Article = () => {
+  const { data, loading } = useRequest(getArticle, {
+    cacheKey: 'cacheKey-demo',
+  });
+  if (!data && loading) {
+    return <p>Loading</p>;
+  }
+  return (
+    <>
+      <p>Background loading: {loading ? 'true' : 'false'}</p>
+      <p>Latest request time: {data?.time}</p>
+      <p>{data?.data}</p>
+    </>
+  );
+};
+
+export default () => {
+  const [state, { toggle }] = useBoolean();
+  return (
+    <div>
+      <button type="button" onClick={() => toggle()}>
+        show/hidden
+      </button>
+      {state && <Article />}  
+    </div>
+  );
+};
---
+++ src/client/dong.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState, useRef, useLayoutEffect } from 'react';
+
+function Dong() {
+
+    const [count, setCount] = useState(0);
+
+    // useEffect(() => {
+    //     const timer = setInterval(() => {
+    //         setCount(count + 1);
+    //     }, 500);
+    //     clearInterval(timer);
+    // }, []);
+
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         console.log(count);
+    //     }, 500);
+    // }, []);
+
+    // useEffect(() => {
+    //     const timer = setInterval(() => {
+    //         setCount(count + 1);
+    //     }, 500);
+    //     console.log(count, 'count');
+    //     return () => {
+    //         clearInterval(timer);
+    //     }
+    // }, [count]);
+
+    // useEffect(() => {
+    //     const timer = setInterval(() => {
+    //         console.log(count);
+    //     }, 500);
+    //     return () => {
+    //         clearInterval(timer);
+    //     }
+    // }, [count]);
+
+    useEffect(() => {
+        setInterval(() => {
+            setCount(count => count + 1);
+        }, 500);
+    }, []);
+
+
+    const fn = () => {
+        console.log(count);
+    };
+    const ref = useRef(fn);
+
+    useLayoutEffect(() => {
+        ref.current = fn;
+    });
+
+    useEffect(() => {
+        setInterval(() => ref.current(), 500);
+    }, []);
+
+    return <div>{count}</div>;
+}
+
+export default Dong;
