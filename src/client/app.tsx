d9178caaa2f295ba127e130bca2a9b63aea50bba
fortunegrant <1209095474@qq.com>
2023-06-04 23:09:43 +0800

    feat: update

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,38 +1,54 @@
-import React from 'react';
-import './index.less';
-import Father from './useImperativeHandle';
-import Test from './useReducer';
-import Forward from './forwardRef';
-import { ATest1, ATest2 } from './hoc';
+import React from "react";
+import "./index.less";
+import Father from "./useImperativeHandle";
+import Test from "./useReducer";
+import Forward from "./forwardRef";
+import { ATest1, ATest2 } from "./hoc";
 // import SuspenseDemo from './suspenseDemo';
-import SuspensePacking from './suspensePacking';
-import TestAsync from './useAsync';
-import Index from './useIndex';
-import CC from './classComponent';
-import Dong from './dong';
-import Ahooks from './ahooks';
-import Boom from './boom';
+import SuspensePacking from "./suspensePacking";
+import TestAsync from "./useAsync";
+import Index from "./useIndex";
+import CC from "./classComponent";
+import Dong from "./dong";
+import Ahooks from "./ahooks";
+import Boom from "./boom";
+import Demo from "./useLayoutEffect";
 
 // import  from 'monaco-editor';
 
 export default class App extends React.Component {
-    render() {
-        return <div>
-            <div className='header'>222</div>
-            <div id='container'></div>
-            {/* <Father />
+  render() {
+    return (
+      <div>
+        <div className="header">222</div>
+        <div id="container"></div>
+        {/* <Father />
             <Forward />
             <Test />
             <ATest1 />
             <ATest2 />
             <SuspensePacking /> */}
-            {/* <MonacoEditor /> */}
-            <div><TestAsync /></div>
-            <div><Index /></div>
-            <div><CC /></div>
-            <div><Ahooks /></div>
-            <div><Boom isShow /></div>
-            {/* <div><Dong /></div> */}
+        {/* <MonacoEditor /> */}
+        <div>
+          <TestAsync />
         </div>
-    }
+        <div>
+          <Index />
+        </div>
+        <div>
+          <CC />
+        </div>
+        <div>
+          <Ahooks />
+        </div>
+        <div>
+          <Boom isShow />
+        </div>
+        {/* <div><Dong /></div> */}
+        <div>
+          <Demo />
+        </div>
+      </div>
+    );
+  }
 }
