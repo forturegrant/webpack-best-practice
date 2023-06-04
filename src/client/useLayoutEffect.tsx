79526a366f144234175e26e963648245aa9caa3a
fortunegrant <1209095474@qq.com>
2023-06-04 23:09:48 +0800

    feat: update

---
+++ src/client/useLayoutEffect.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useLayoutEffect } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+
+  // useLayoutEffect(() => {
+  useEffect(() => {
+    if (count === 0) {
+      const randomNum = 10 + Math.random() * 200;
+      setCount(randomNum);
+    }
+  }, [count]);
+
+  return <div onClick={() => setCount(0)}>{count}</div>;
+}
+
+export default App;
