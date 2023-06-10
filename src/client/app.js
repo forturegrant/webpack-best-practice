9147acae3fe59800c225589b235b8aa8d24639a4
fortunegrant <1209095474@qq.com>
2023-06-10 21:40:24 +0800

    feat: update

---
+++ src/client/app.js
@@ -0,0 +1,7 @@
+import React from "rhttps://github.com/forturegrant/hanjin-nice.giteact";
+
+export default class App extends React.Component {
+  render() {
+    return <div>吼吼吼吼吼吼吼吼吼吼吼吼吼吼吼</div>;
+  }
+}
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,21 +1,41 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    target: 'node',
-    entry: path.join(__dirname, 'src/client/ServerApp'),
-    output: {
-        filename: 'ServerApp.js',
-        path: `${__dirname}/dist`,
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    }
-}
+  mode: "development",
+  target: "node",
+  entry: path.join(__dirname, "src/client/ServerApp"),
+  output: {
+    filename: "ServerApp.js",
+    path: `${__dirname}/dist`,
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        // 命中 less 文件
+        test: /\.less$/,
+        // 从右到左依次使用 less-loader、css-loader、style-loader
+        use: ["style-loader", "css-loader", "less-loader"],
+        // 排除 node_modules 下面的 less 文件
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.png|jpg|gif|jpeg|svg/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024,
+          },
+        },
+        generator: {
+          filename: "images/[base]",
+        },
+      },
+    ],
+  },
+};
