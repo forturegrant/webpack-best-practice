6f26b0200f3a084afe8c38d233fb3b97a702eba1
fortunegrant <1209095474@qq.com>
2023-06-18 22:30:54 +0800

    feat: update

--- src/client/IPromise.ts
+++ src/client/IPromise.ts
@@ -11,3 +11,15 @@ const getName = (): Promise<Result> =>
   Promise.resolve({
     name: "zzp",
   });
+
+function fetchData<T>(url: string): Promise<T> {
+  return fetch(url).then((res) => res.json());
+}
+
+interface User {
+  name: string;
+}
+
+fetchData<User>("https://api.example.com/users/1").then((user) =>
+  console.log(user.name)
+);
